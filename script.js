d2fed4015c2df5357fe8c06f2d085bd11ee5d015
Feruza <feruzarustamova2015@gmail.com>
2024-07-02 07:19:49 +0500

    contact-from project

---
+++ script.js
@@ -0,0 +1,8 @@
+let inputText = document.querySelector('.input-text ')
+let sumText = document.querySelector('.sum-text')
+// console.log(inputText )
+sumText.addEventListener('click', function () {
+    
+    inputText == "red"
+
+})
